c92381ff228136c087adab3109c5744561fe11a2
devsoupe <luvyum4e@gmail.com>
2023-06-23 23:16:23 +0900

    first commit

---
+++ src/main/java/hello/thymeleaf/KyhSpringMvc2ThymeleafBasicApplication.java
@@ -0,0 +1,13 @@
+package hello.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyhSpringMvc2ThymeleafBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KyhSpringMvc2ThymeleafBasicApplication.class, args);
+    }
+
+}
